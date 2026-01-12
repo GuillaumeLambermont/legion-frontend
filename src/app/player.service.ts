c048d1d8373db28e7d298131efcf9d15c2aaeb7c
GuillaumeLambermont <guillaume.lambermont@gmail.com>
2026-01-12 15:10:33 +0100

    Switch api url to env dev

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -2,15 +2,16 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Player } from './player.model';
+import { environment } from '../environments/environment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/players';
+  private apiServerUrl = environment.apiUrl;
 
   getPlayers(): Observable<Player[]> {
-    return this.http.get<Player[]>(this.apiUrl);
+    return this.http.get<Player[]>(`${this.apiServerUrl}/players`);
   }
 }
