0ebaf8203b67bf43e0fc9e01ecbc220a5d35bbea
Guillaume Lambermont <guillaume.lambermont@gmail.com>
2026-02-17 17:15:19 +0100

    Added environments

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -2,13 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Player } from './player.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:8080/players';
+  private apiUrl = `${environment.apiUrl}/players`
 
   getPlayers(): Observable<Player[]> {
     return this.http.get<Player[]>(this.apiUrl);
