f052e79b951e7414d18bd9108a869e534c7bbeb7
Guillaume Lambermont <guillaume.lambermont@gmail.com>
2026-03-21 06:26:50 +0100

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:8080',
+  apiUrl: 'http://localhost:8080',
   keycloakClientId: 'legion-backend-dev', // Will be replaced
   keycloakRealm: 'legion-dev',           // Will be replaced
   keycloakUrl: 'http://192.168.1.41:8080'
